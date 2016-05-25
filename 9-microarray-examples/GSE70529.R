4b74e927b13184e9daaff1c100f8fce30c6f9c10
jimmy_zeng <ulwvfje@gmail.com>
2016-05-25 10:41:27 +0800

    HuGene-1_0-st // GPL6244 // obesity//paired

--- 9-microarray-examples/GSE70529.R
+++ 9-microarray-examples/GSE70529.R
@@ -1,4 +1,6 @@
-## HuGene-1_0-st // GPL6244 // obesity
+## HuGene-1_0-st // GPL6244 // obesity 
+## paired or not 
+rm(list=ls())
 library(GEOquery)
 library(limma) 
 GSE70529 <- getGEO('GSE70529', destdir=".",getGPL = F)
@@ -28,7 +30,7 @@ exprSet=rmDupID(a)
 rn=rownames(exprSet)
 exprSet=apply(exprSet,2,as.numeric)
 rownames(exprSet)=rn
-
+exprSet[1:4,1:4]
 #exprSet=log(exprSet)
 #boxplot(exprSet,las=2)
 
@@ -44,8 +46,31 @@ design=model.matrix(~treatment)
 fit=lmFit(exprSet,design)
 fit=eBayes(fit) 
 #vennDiagram(fit,include=c("up","down"))
-write.csv(topTable(fit,coef='treatmentB',n=Inf,adjust='BH'),"treatmentBvsA.DEG.csv")
-write.csv(topTable(fit,coef='treatmentC',n=Inf,adjust='BH'),"treatmentCvsA.DEG.csv")
-write.csv(topTable(fit,coef='treatmentD',n=Inf,adjust='BH'),"treatmentDvsA.DEG.csv")
-## and we don't care about the difference between individuals
+write.csv(topTable(fit,coef='treatmentB',n=Inf,adjust='BH'),"BvsA_NonPaired.DEG.csv")
+write.csv(topTable(fit,coef='treatmentC',n=Inf,adjust='BH'),"CvsA_NonPaired.DEG.csv")
+write.csv(topTable(fit,coef='treatmentD',n=Inf,adjust='BH'),"DvsA_NonPaired.DEG.csv")
+BvsA_NonPaired=topTable(fit,coef='treatmentB',n=Inf,adjust='BH')
+CvsA_NonPaired=topTable(fit,coef='treatmentC',n=Inf,adjust='BH')
+DvsA_NonPaired=topTable(fit,coef='treatmentD',n=Inf,adjust='BH')
+
+
+## if paired analysis :
+design=model.matrix(~individuals+treatment)
+fit=lmFit(exprSet,design)
+fit=eBayes(fit)  
+write.csv(topTable(fit,coef='treatmentB',n=Inf,adjust='BH'),"BvsA_Paired.DEG.csv")
+write.csv(topTable(fit,coef='treatmentC',n=Inf,adjust='BH'),"CvsA_Paired.DEG.csv")
+write.csv(topTable(fit,coef='treatmentD',n=Inf,adjust='BH'),"DvsA_Paired.DEG.csv")
+BvsA_Paired=topTable(fit,coef='treatmentB',n=Inf,adjust='BH')
+CvsA_Paired=topTable(fit,coef='treatmentC',n=Inf,adjust='BH')
+DvsA_Paired=topTable(fit,coef='treatmentD',n=Inf,adjust='BH')
+
+
+t1=rownames(DvsA_Paired[DvsA_Paired$P.Value<0.05,])
+t2=rownames(DvsA_NonPaired[DvsA_NonPaired$P.Value<0.05,])
+length(setdiff(t1,t2))   ## 594
+length(setdiff(t2,t1))   ## 16 
+length(intersect(t1,t2))  ##1131
+ 
+
 
